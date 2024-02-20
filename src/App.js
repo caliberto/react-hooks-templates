3740809b1710d8ce4784280379d1724f2567ad7d
ciro.aliberto <ciro.aliberto@finconsgroup.com>
2024-02-20 16:54:26 +0100

    fixed libs

--- src/App.js
+++ src/App.js
@@ -2,15 +2,8 @@ import React from 'react';
 
 import AppContainer from './router/AppContainer';
 
-import { AppStateProvider } from './context/store/AppState';
-import { appReducers, initialState } from './context/reducers/config';
-
 function App() {
-  return (
-    <AppStateProvider reducer={appReducers} initialState={initialState}>
-      <AppContainer />
-    </AppStateProvider>
-  );
+  return <AppContainer />;
 }
 
 export default App;
